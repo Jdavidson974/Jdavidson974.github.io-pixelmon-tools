90aa490b09bbee2748d36038f14253ef3d53759c
JD <jd9743101@gmail.com>
2024-10-10 22:42:20 +0200

    maj angular + ngrx

--- src/app/bingo/bingo.component.ts
+++ src/app/bingo/bingo.component.ts
@@ -1,11 +1,15 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { BingoService } from './services/bingo.service';
 
 @Component({
   selector: 'app-bingo',
   standalone: true,
-  imports: [HttpClientModule,],
+  imports: [
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule,],
   providers: [BingoService],
   templateUrl: './bingo.component.html',
   styleUrl: './bingo.component.scss'
