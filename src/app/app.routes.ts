aee814840bf09ed2137ad97dfa71bb96ce586672
JD <jd9743101@gmail.com>
2024-10-10 17:08:34 +0200

    navbar + home + bingo components + scss architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BingoComponent } from './bingo/bingo.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "bingo", component: BingoComponent },
+    { path: "", component: HomeComponent }
+];
---
+++ src/app/bingo/bingo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bingo',
+  standalone: true,
+  imports: [],
+  templateUrl: './bingo.component.html',
+  styleUrl: './bingo.component.scss'
+})
+export class BingoComponent {
+
+}
