b777cc6cfd61e80f9e7f3d1cb9bed29dda983d94
JD <jd9743101@gmail.com>
2024-10-31 15:36:37 +0100

    ajout du dark mode + du bouton sroll up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { ScrollTopDirective } from './directives/scroll-top.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent,],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, ScrollTopDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Pixelmon-tools';
+
+  toggleDarkTheme(): void {
+    document.body.classList.toggle('dark-theme');
+  }
 }
---
+++ src/app/directives/scroll-top.directive.ts
@@ -0,0 +1,33 @@
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appScrollTop]',
+  standalone: true
+})
+export class ScrollTopDirective {
+
+  constructor(private el: ElementRef, private renderer2: Renderer2) { }
+
+  @HostListener('window:scroll', ['$event'])
+  toggleVisibility(event: Event) {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    if (scrollPosition >= 800) {
+      console.log("toto");
+      console.log(this.el.nativeElement);
+
+      this.renderer2.removeClass(this.el.nativeElement, "hide");
+    } else {
+
+      this.renderer2.addClass(this.el.nativeElement, "hide")
+    }
+  }
+
+  @HostListener('click')
+  scrollTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+}
